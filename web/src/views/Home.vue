01a2000a0c28a73643b67a4d93e976056640cad0
李岩 <541267924@qq.com>
2020-05-10 18:38:04 +0800

    init

---
+++ web/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      active: 0
+    };
+  }
+};
+</script>
